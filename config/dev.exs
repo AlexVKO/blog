07b02848d4ceebfd3b56d9a2a941631280591431
Alex VKO <ale@alexvko.com>
2017-01-16 12:52:49 -0200

    Sets postgres for dev

--- config/dev.exs
+++ config/dev.exs
@@ -36,8 +36,8 @@ config :phoenix, :stacktrace_depth, 20
 # Configure your database
 config :blog, Blog.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  # username: "postgres",
+  # password: "postgres",
   database: "blog_dev",
   hostname: "localhost",
   pool_size: 10
